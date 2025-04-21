11ea5d3be63e4d5e01859729daa5d3a8a60376a7
Kevin Estrada <125918656+KevEstr@users.noreply.github.com>
2025-04-20 21:49:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ app.ticket_email_service = TicketEmailService(azure_connection_string, sender_ad
 register_blueprints(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto asignado por Railway
+    app.run(host="0.0.0.0", port=port, debug=True)  # Escucha en todas las interfaces
