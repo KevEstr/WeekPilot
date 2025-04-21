70a1b2a8b510d66c28110299b3b562e1e36a08e8
Kevin Estrada <125918656+KevEstr@users.noreply.github.com>
2025-04-21 11:18:54 -0500

    Update queries.py

--- services/queries.py
+++ services/queries.py
@@ -10,8 +10,8 @@ engine = create_engine(
     'mHRL_%3C%3D%27%28%5D%2C%23aZ%29T%22A3QeD'    # contraseña percent‑encoded
     '@20.109.21.246:1433'
     '/MICELU'
-    echo=False,           # True para debug SQL
-    pool_pre_ping=True,   # chequea conectividad antes de cada uso
+    echo=False,          # True para debug SQL
+    pool_pre_ping=True   # chequea conectividad antes de cada uso
 )
 
 
