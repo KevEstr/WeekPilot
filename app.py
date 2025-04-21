24dbf4007a2538f34aa874475903a8a0a0c01296
Kevin Estrada <125918656+KevEstr@users.noreply.github.com>
2025-04-21 10:33:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -68,5 +68,5 @@ register_blueprints(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
-     port = int(os.environ.get("PORT", 5000))  
-     app.run(host="0.0.0.0", port=port, debug=True) 
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True) 
